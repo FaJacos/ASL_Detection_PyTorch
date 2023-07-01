b15a1b1d54a97bd1b38184db4904b29ca99f26f2
FaJacos <Jacob234lin@gmail.com>
2023-07-01 12:51:47 -0400

    hand detection framing

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import time
 import torch as nn
 import model
 
+#Intialize variables for hand detection
+mpHands = mp.solutions.hands
+hands =  mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
 #Selects either GPU or CPU to run on
 device = nn.device("cuda" if nn.cuda.is_available() else "cpu")
 
@@ -16,8 +21,21 @@ numEpoch = 10
 
 #Loading data
 
+#Takes in a image and returns a 200x200 photo of the hand with the palm as the midpoint
+def handDetector(image):
+    imgRGB = cv.cvtColor(image,cv.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    handDetect = results.multi_hand_landmarks
+    
+    if handDetect:
+        handDetect[0]
+    else:
+        print("Hand not detected")
+
+    
 
 #Intialize network
 neuroNetwork = model.network(numNodes = numNodes, numClasses = numClasses).to(device)
+
 
 
